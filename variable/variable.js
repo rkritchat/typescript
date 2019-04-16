9d28e2f701a22cfa275db6809c66dd130666fd49
rkritchat <kritchat@orcsoft.co.th>
2019-04-16 14:02:29 +0700

    create varaible

---
+++ variable/variable.js
@@ -0,0 +1,9 @@
+var a;
+var b;
+var c;
+//Valid in typescritp
+a = 10;
+b = 'test';
+c = true;
+//Invalid in tpye script but can compile to javascript, *compile with warinning
+a = 'test';
---
+++ variable/variable.ts
@@ -0,0 +1,14 @@
+var a:number
+var b:string
+var c:boolean
+
+
+//Valid in typescritp
+a = 10
+b = 'test'
+c = true
+
+//Invalid in tpye script but can compile to javascript, *compile with warinning
+a = 'test'
+
+
